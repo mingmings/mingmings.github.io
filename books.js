07b556f812ce4c6a632c50f851913c9f8cd0f5e4
mingmings <369413651@qq.com>
2015-06-06 03:26:17 +0800

    修改标题

--- books.js
+++ books.js
@@ -5,13 +5,35 @@ var books = [
     { name: 'Redis 设计与实现',
         url: imgUrl + 'redis she ji yu shi xian.jpg', type: '后端', reading: 1 },
 
+
     { name: 'Head First Python',
         url: imgUrl + 'head first python.jpg', type: '后端', reading: 2 },
+
 
     { name: 'Head First Python',
         url: imgUrl + 'head first python.jpg', type: '后端', reading: 3 },
     { name: '向前一步',
         url: imgUrl + 'lean in.jpg', type: '人物传记', reading: 3 },
+    { name: '史蒂夫·乔布斯传',
+        url: imgUrl + '斯蒂文乔布斯传.jpg', type: '人物传记', reading: 3 },
+    { name: '看见',
+        url: imgUrl + '看见.jpg', type: '人物传记', reading: 3 },
+    { name: '不能承受的生命之轻',
+        url: imgUrl + '不能承受的生命之轻.jpg', type: '小说', reading: 3 },
+    { name: '源泉',
+        url: imgUrl + '源泉.jpg', type: '小说', reading: 3 },
+    { name: '疯狂的程序员',
+        url: imgUrl + '疯狂的程序员.jpg', type: '小说', reading: 3 },
+    { name: '轻风茗月品女人',
+        url: imgUrl + '轻风茗月品女人.jpg', type: '散文', reading: 3 },
+    { name: '波利泰里尼网球手册',
+        url: imgUrl + '波利泰里尼网球手册.jpg', type: '运动健身', reading: 3 },
+    { name: '肌肉健美图解',
+        url: imgUrl + '肌肉健美图解.jpg', type: '运动健身', reading: 3 },
     { name: '罗织经',
-        url: imgUrl + '罗织经.jpg', type: '其他', reading: 3 }
+        url: imgUrl + '罗织经.jpg', type: '其他', reading: 3 },
+    { name: '没有任何借口',
+        url: imgUrl + '没有任何借口.jpg', type: '其他', reading: 3 },
+    { name: '高效能人士的七个习惯',
+        url: imgUrl + '高效能人士的七个习惯.jpg', type: '其他', reading: 3 }
 ];

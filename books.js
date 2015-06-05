1469ec1f02a162ffcc79ad96165b1058e8233124
mingmings <369413651@qq.com>
2015-06-05 19:00:01 +0800

    ..

--- books.js
+++ books.js
@@ -2,12 +2,15 @@ var imgUrl = 'http://mingmings.qiniudn.com/book/';
 /* 1：在看 2：待看 3 归档 */
 
 var books = [
+    { name: 'Redis 设计与实现',
+        url: imgUrl + 'redis she ji yu shi xian.jpg', type: '后端', reading: 1 },
+
+    { name: 'Head First Python',
+        url: imgUrl + 'head first python.jpg', type: '后端', reading: 2 },
+
     { name: 'Head First Python',
         url: imgUrl + 'head first python.jpg', type: '后端', reading: 3 },
 
-    { name: 'HTML5和JavaScript Web应用开发',
-        url: imgUrl + '2.jpg', type: '前端', reading: 3 },
-
     { name: '向前一步',
-        url: imgUrl + '向前一步.jpg', type: '人物', reading: 3 },
+        url: imgUrl + 'lean in.jpg', type: '人物传记', reading: 3 }
 ];

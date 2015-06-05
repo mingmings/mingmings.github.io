266b199dea850ad14e22274224ee4e671bfec0bd
mingmings <369413651@qq.com>
2015-06-05 18:10:50 +0800

    增加bookcase

---
+++ books.js
@@ -0,0 +1,13 @@
+var imgUrl = 'http://mingmings.qiniudn.com/book/';
+/* 1：在看 2：待看 3 归档 */
+
+var books = [
+    { name: 'Head First Python',
+        url: imgUrl + 'head first python.jpg', type: '后端', reading: 3 },
+
+    { name: 'HTML5和JavaScript Web应用开发',
+        url: imgUrl + '2.jpg', type: '前端', reading: 3 },
+
+    { name: '向前一步',
+        url: imgUrl + '向前一步.jpg', type: '人物', reading: 3 },
+];

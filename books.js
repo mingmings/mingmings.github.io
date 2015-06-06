ab724ac097f7a514f101ede59a3b9574dbf99ba3
mingmings <369413651@qq.com>
2015-06-06 11:30:16 +0800

    add scripts

--- books.js
+++ books.js
@@ -15,27 +15,31 @@ var books = [
         url: imgUrl + 'mongodb实战.jpg', type: '后端', reading: 2 },
     { name: 'Mongodb 权威指南',
         url: imgUrl + 'mongodb权威指南.jpg', type: '后端', reading: 2 },
-
     { name: 'Redis 入门指南',
         url: imgUrl + 'redis 入门指南.jpg', type: '后端', reading: 3 },
+
     { name: '向前一步',
         url: imgUrl + '向前一步.jpg', type: '人物传记', reading: 3 },
     { name: '史蒂夫·乔布斯传',
         url: imgUrl + '斯蒂文乔布斯传.jpg', type: '人物传记', reading: 3 },
     { name: '看见',
         url: imgUrl + '看见.jpg', type: '人物传记', reading: 3 },
+
     { name: '不能承受的生命之轻',
         url: imgUrl + '不能承受的生命之轻.jpg', type: '小说', reading: 3 },
     { name: '源泉',
         url: imgUrl + '源泉.jpg', type: '小说', reading: 3 },
     { name: '疯狂的程序员',
         url: imgUrl + '疯狂的程序员.jpg', type: '小说', reading: 3 },
+
     { name: '轻风茗月品女人',
         url: imgUrl + '轻风茗月品女人.jpg', type: '散文', reading: 3 },
+
     { name: '波利泰里尼网球手册',
         url: imgUrl + '波利泰里尼网球手册.jpg', type: '运动健身', reading: 3 },
     { name: '肌肉健美图解',
         url: imgUrl + '肌肉健美图解.jpg', type: '运动健身', reading: 3 },
+
     { name: '罗织经',
         url: imgUrl + '罗织经.jpg', type: '其他', reading: 3 },
     { name: '没有任何借口',
